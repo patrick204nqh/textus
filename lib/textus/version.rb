d0ef979c0c0a114392bec9e56be48adb14fb90a8
Patrick <patrick204nqh@gmail.com>
2026-05-21 22:35:47 +0700

    chore: bump to 0.8.1

--- lib/textus/version.rb
+++ lib/textus/version.rb
@@ -1,4 +1,4 @@
 module Textus
-  VERSION = "0.8.0"
+  VERSION = "0.8.1"
   PROTOCOL = "textus/2"
 end
---
+++ spec/version_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe Textus do
+  it "is at v0.8.1" do
+    expect(Textus::VERSION).to eq("0.8.1")
+  end
+end
