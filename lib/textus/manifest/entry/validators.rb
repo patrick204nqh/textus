4f1e3c5d0c0ad6149dd31d3f9535a0e87be83b04
Patrick <60766722+patrick204nqh@users.noreply.github.com>
2026-05-26 14:50:04 +0700

    feat(manifest): extract Validators::Events from Manifest::Entry

--- lib/textus/manifest/entry/validators.rb
+++ lib/textus/manifest/entry/validators.rb
@@ -2,8 +2,9 @@ module Textus
   class Manifest
     class Entry
       module Validators
-        # Filled in as validators are added.
-        REGISTERED = [].freeze
+        REGISTERED = [
+          Events,
+        ].freeze
 
         def self.run_all(entry)
           REGISTERED.each { |v| v.call(entry) }
