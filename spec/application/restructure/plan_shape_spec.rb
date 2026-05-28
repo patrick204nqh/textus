4a304ee3fc7b9b9f4961badf7a0b9d6a9cb0db50
Patrick <60766722+patrick204nqh@users.noreply.github.com>
2026-05-28 11:46:51 +0700

    feat(restructure): module skeleton with Plan value object

---
+++ spec/application/restructure/plan_shape_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+RSpec.describe Textus::Application::Restructure do
+  it "exposes Plan as a Data class with steps and warnings" do
+    plan = described_class::Plan.new(steps: [{ "op" => "mv", "from" => "a", "to" => "b" }], warnings: ["w1"])
+    expect(plan.steps).to eq([{ "op" => "mv", "from" => "a", "to" => "b" }])
+    expect(plan.warnings).to eq(["w1"])
+  end
+
+  it "Plan#to_h returns a JSON-encodable hash" do
+    plan = described_class::Plan.new(steps: [], warnings: [])
+    expect(plan.to_h).to eq("steps" => [], "warnings" => [])
+  end
+end
