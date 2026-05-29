f53ebb72383845f1dbfa40fa769b97074207ee71
Patrick <60766722+patrick204nqh@users.noreply.github.com>
2026-05-29 07:14:17 +0700

    refactor(where): collapse Module+Impl to plain class

--- lib/textus/application/read/where.rb
+++ lib/textus/application/read/where.rb
@@ -1,22 +1,16 @@
 module Textus
   module Application
     module Read
-      module Where
-        def self.call(*, session:, ctx:, caps:, **) # rubocop:disable Lint/UnusedMethodArgument
-          Impl.new(caps: caps).call(*, **)
+      class Where
+        def initialize(container:, call: nil, hook_context: nil) # rubocop:disable Lint/UnusedMethodArgument
+          @manifest = container.manifest
         end
 
-        class Impl
-          def initialize(caps:)
-            @manifest = caps.manifest
-          end
-
-          def call(key)
-            res = @manifest.resolver.resolve(key)
-            mentry = res.entry
-            path = res.path
-            { "protocol" => PROTOCOL, "key" => key, "zone" => mentry.zone, "owner" => mentry.owner, "path" => path }
-          end
+        def call(key)
+          res = @manifest.resolver.resolve(key)
+          mentry = res.entry
+          path = res.path
+          { "protocol" => PROTOCOL, "key" => key, "zone" => mentry.zone, "owner" => mentry.owner, "path" => path }
         end
       end
     end
